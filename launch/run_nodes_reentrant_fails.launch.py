07f9ddd624587c717ca40dd2380caaa399cba4f0
Joe Schornak <joe.schornak@gmail.com>
2020-10-12 12:09:15 -0500

    rename package to clarify purpose

--- launch/run_nodes_reentrant_fails.launch.py
+++ launch/run_nodes_reentrant_fails.launch.py
@@ -5,7 +5,7 @@ import launch_ros.actions
 
 def generate_launch_description():
     ld = LaunchDescription([
-        launch_ros.actions.Node(node_name='server_node', package='action_test', node_executable='action_test_server_node', output='screen'),
-        launch_ros.actions.Node(node_name='client_node', package='action_test', node_executable='action_test_client_node_multithread_reentrant', output='screen'),
+        launch_ros.actions.Node(node_name='server_node', package='action_exception_test', node_executable='action_exception_test_server_node', output='screen'),
+        launch_ros.actions.Node(node_name='client_node', package='action_exception_test', node_executable='action_exception_test_client_node_multithread_reentrant', output='screen'),
     ])
     return ld
